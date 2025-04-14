ff85356ef577ef006b9d3ed50b0e29edbefc0851
harsh <harsh.chourasia@tatvasoft.com>
2025-04-14 11:37:22 +0530

    initial api-gateway and shared-lib

---
+++ api-gateway/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+
+@Module({
+  imports: [AuthModule, UserModule, ProductModule, OrderModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
